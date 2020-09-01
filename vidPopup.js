04bb2e354b18c26d24acf5f75f9dad34c798ffb2
jpzp <43419134+jpzp@users.noreply.github.com>
2020-09-01 18:55:46 -0400

    popup video

--- vidPopup.js
+++ vidPopup.js
@@ -1,1 +1,16 @@
-  //Video popup code
+// Video popup code
+const overlay = document.querySelector('#overlay');
+
+// Make popup work for one card then generalize to all of them
+const crazyCar = document.querySelector('#crazy-car-img');
+
+// onClick grey div and iframe video;
+// x in top corner (create and style div, make visible, then just add iframe ontop in here?)
+crazyCar.addEventListener('click', ()=>{
+  overlay.style.display = "block";
+  /*
+  const frame = document.createElement('iframe');
+  frame.src= "https://www.youtube.com/embed/3N1oi_833kk";
+  frame.
+  */
+})
